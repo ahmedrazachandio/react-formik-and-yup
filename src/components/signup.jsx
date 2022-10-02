b3a01815397177b855d1e3d1005ea492a96412ad
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-02 23:28:20 +0500

    code beautify

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,44 +1,44 @@
-import { Formik , Field , Form } from 'formik';
+import { Formik, Field, Form } from 'formik';
 
 
 
 
 
 function Signup() {
-    return (
-        <div className="container">
-        <h1>Signup</h1>
-        <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field id="firstName" name="firstName" placeholder="Jane" />
+  return (
+    <div className="container">
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
 
-        <label htmlFor="lastName">Last Name</label>
-        <Field id="lastName" name="lastName" placeholder="Doe" />
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
 
-        <label htmlFor="email">Email</label>
-        <Field
-          id="email"
-          name="email"
-          placeholder="jane@acme.com"
-          type="email"
-        />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
 
 
-        {/* <form>
+      {/* <form>
           <div className="form-control">
             <input type="text" id="fullName" required />
             <label>Full Name</label>
@@ -65,8 +65,8 @@ function Signup() {
   
           <p className="text">You have an account <a href="index.html">Login</a> </p>
         </form> */}
-      </div>
-    );
-  }
-  
-  export default Signup;
+    </div>
+  );
+}
+
+export default Signup;
