2822d1ef562a2f8aa7a49b86b34c5bcaea8f444d
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-02 23:24:52 +0500

    example code paste

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -8,7 +8,37 @@ function Signup() {
     return (
         <div className="container">
         <h1>Signup</h1>
-        <form>
+        <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="Jane" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+
+
+        {/* <form>
           <div className="form-control">
             <input type="text" id="fullName" required />
             <label>Full Name</label>
@@ -34,7 +64,7 @@ function Signup() {
           <button className="btn" type="submit" onclick="signUp()">Signup</button>
   
           <p className="text">You have an account <a href="index.html">Login</a> </p>
-        </form>
+        </form> */}
       </div>
     );
   }
